7d50fec00247f7d666fb3fa7b395592706ae0223
Eric Baxter <ericjbax@gmail.com>
2017-09-03 15:00:35 -0400

    cosmetic changes to code

--- src/js/interpolator.js
+++ src/js/interpolator.js
@@ -5,32 +5,32 @@ function interpolate(param, param_table, func_table) {
     //   dimension is (len(param_table), len(P(k)))
 
     //Determine indices that bound input value
-    index1 = 0
-    index2 = 1
+    lowerindex = 0;
+    upperindex = 1;
     for (i = 0; i < param_table.length; i++){
 	if ((i > 1) && (param_table[i] > param)){
-	    index1 = param_table[i-1];
-	    index2 = param_table[i];
+	    lowerindex = param_table[i-1];
+	    upperindex = param_table[i];
 	    break;
 	}
     }
-    if ((param_table[index1] =< param) && (param_table[index2] >= param)){
+    if ((param_table[lowerindex] =< param) && (param_table[upperindex] >= param)){
 	//success
     }
     else{
 	//error!
     }
 
-    //Determine weights for function at index1 and index2
+    //Determine weights for function at lowerindex and upperindex
     //double check this
-    weight1 = (param_table[index2]-param)/(param_table[index2] - param_table[index1]);
-    weight2 = (param - param_table[index1])/(param_table[index2] - param_table[index1]);
+    weight1 = (param_table[upperindex]-param)/(param_table[upperindex] - param_table[lowerindex]);
+    weight2 = (param - param_table[lowerindex])/(param_table[upperindex] - param_table[lowerindex]);
 
     //Generate output array
     //is this the right way to do this?
     output_array = [];
     for (i = 0; i < func_table[0].length; i++){
-	new_val = weight1*func_table[index1,i] + weight2*func_table[index2,i];
+	new_val = weight1*func_table[lowerindex,i] + weight2*func_table[upperindex,i];
 	output_array.push(new_val);
     }
 
@@ -40,23 +40,23 @@ function interpolate(param, param_table, func_table) {
 
 //Interpolate 1D function, useful for plotting
 function interpolate1d(x_out, x_in, y_in){
-    index1 = 0
-    index2 = 1
+    lowerindex = 0;
+    upperindex = 1;
     
     y_out = []
     for (i = 0; i < x_out.length; i++){
 	x_desired = x_out[i];
-	for (i = index1; i < param_table.length; i++){
+	for (i = lowerindex; i < param_table.length; i++){
 	    if ((i > 1) && (x_in[i] > x_desired)){
-		index1 = param_table[i-1];
-		index2 = param_table[i];
+		lowerindex = param_table[i-1];
+		upperindex = param_table[i];
 		break;
 	    }
 	}
 	
-	weight1 = (x_in[index2]-x_desired)/(x_in[index2] - x_in[index1]);
-	weight2 = (x_desired - x_in[index1])/(x_in[index2] - x_in[index1]);
-	y_out_i =  weight1*y_in[index1,i] + weight2*y_in[index2,i];
+	weight1 = (x_in[upperindex]-x_desired)/(x_in[upperindex] - x_in[lowerindex]);
+	weight2 = (x_desired - x_in[lowerindex])/(x_in[upperindex] - x_in[lowerindex]);
+	y_out_i =  weight1*y_in[lowerindex,i] + weight2*y_in[upperindex,i];
 	y_out.push(y_out_i);
     }
 
