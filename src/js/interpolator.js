34dc9a06c777bdf7e23c8922313695ce19f2e0dc
Eric Baxter <ericjbax@gmail.com>
2017-09-03 14:26:10 -0400

    first commit of interpolator code

---
+++ src/js/interpolator.js
@@ -0,0 +1,6 @@
+function interpolate(param, param_table, func_table) {
+    //param is desired value of parameter (i.e. Omega_M, n_s, Om h^2)
+    //param_table is table of param values for which we've precomputed function
+    //func_table is matrix of function evals (either P(k) or C(ell)
+    //   dimension is (len(param_table), len(P(k)))
+};
