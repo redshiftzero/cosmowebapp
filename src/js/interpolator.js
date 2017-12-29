6593f64f9b1e46fcbce7a67184f98c756b2e3f33
Eric Baxter <ericjbax@gmail.com>
2017-12-28 22:24:27 -0500

    changes to interp

--- src/js/interpolator.js
+++ src/js/interpolator.js
@@ -7,30 +7,41 @@ function interpolate(param, param_table, func_table) {
     //Determine indices that bound input value
     lowerindex = 0;
     upperindex = 1;
-    for (i = 0; i < param_table.length; i++){
-	if ((i > 1) && (param_table[i] > param)){
-	    lowerindex = param_table[i-1];
-	    upperindex = param_table[i];
+    for (i = 1; i < param_table.length; i++){
+	if (param_table[i] > param){
+	    lowerindex = i-1;
+	    upperindex = i;
 	    break;
 	}
     }
-    if ((param_table[lowerindex] =< param) && (param_table[upperindex] >= param)){
+    if ((param_table[lowerindex] <= param) && (param_table[upperindex] >= param)){
 	//success
     }
     else{
 	//error!
     }
 
+    console.log("upper index = ", upperindex);
+    console.log("lower index = ", lowerindex);
+    console.log("param = ", param);
+    console.log("upper = ", param_table[upperindex]);
+    console.log("lower = ", param_table[lowerindex]);
+
     //Determine weights for function at lowerindex and upperindex
     //double check this
     weight1 = (param_table[upperindex]-param)/(param_table[upperindex] - param_table[lowerindex]);
     weight2 = (param - param_table[lowerindex])/(param_table[upperindex] - param_table[lowerindex]);
 
+    console.log("weight 1 = ", weight1);
+    console.log("weight 2 = ", weight2);
+    console.log("fuc table dim0 = ", func_table.length);
+    console.log("fuc table dim1 = ", func_table[0].length);
+    
     //Generate output array
     //is this the right way to do this?
     output_array = [];
     for (i = 0; i < func_table[0].length; i++){
-	new_val = weight1*func_table[lowerindex,i] + weight2*func_table[upperindex,i];
+	new_val = weight1*func_table[lowerindex][i] + weight2*func_table[upperindex][i];
 	output_array.push(new_val);
     }
 
