fb19f08c214c9c99ef6bd5ce0a559384c53a4b15
redshiftzero <jen@freedom.press>
2018-01-01 18:17:58 -0800

    Add initial sliders

---
+++ src/js/sliders.js
@@ -0,0 +1,8 @@
+var slider = document.getElementById("omega-m-slider");
+var sliderValue = document.getElementById("omega-m-range-value");
+
+slider.oninput = function() {
+  var currentValue = this.value;
+  sliderValue.innerHTML = parseFloat(currentValue).toFixed(2);
+  console.log('here we will eventually compute interpolatation and update plot for ' + currentValue)
+}
