ab610a891c3395a7d9b2e92c32ca4291f203517c
Eric Baxter <ericjbax@gmail.com>
2018-01-05 23:36:45 -0500

    adding more sliders and a checkbox

---
+++ src/js/checkboxes.js
@@ -0,0 +1,6 @@
+var checkBox = document.getElementById("fix-omegamh2-checkbox");
+
+checkBox.onclick = function() {
+    fix_omegamh2 = checkBox.checked;
+    console.log("fix omegamh2 = ", fix_omegamh2);
+}
