b960fb7e03f5c46896904b27ffd11bb612abc595
Eric Baxter <root@thegibson.baxapt>
2018-01-12 00:43:48 -0500

    making it work with new data format

--- src/js/checkboxes.js
+++ src/js/checkboxes.js
@@ -3,4 +3,5 @@ var checkBox = document.getElementById("fix-omegamh2-checkbox");
 checkBox.onclick = function() {
     fix_omegamh2 = checkBox.checked;
     console.log("fix omegamh2 = ", fix_omegamh2);
+    run_pk_display();
 }
--- src/js/interpolator.js
+++ src/js/interpolator.js
@@ -1,19 +1,21 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50};
 
+
 //Interpolate P(k) table and plot
 function run_interpolation(){
-
-    //Needed to get k array
-    pk_initial = get_k_pk_from_table(pk_table, 0)
-
+   
     //Set up plot axes
     g = plot_axes(margin);
+
+    //console.log("param table = ", param_table);
 
     var pk_interp = interpolate(paramValue, param_table, pk_table) ;
     //create new array with k and P(k) for plotting
     var pk_to_plot = [];
     for (var i = 0; i < pk_interp.length; i++){
-	pk_to_plot.push([pk_initial[i][0], pk_interp[i]]);
+	//console.log("test = ", pk_initial[i][0]);
+	//console.log("length = ", pk_interp.length);
+	pk_to_plot.push([pk_table[i][0], pk_interp[i]]);
     }
 
     //Now plot interpolated P(k)
@@ -25,43 +27,31 @@ function run_interpolation(){
 function process_pk_table(error, textData){
     if (error) return console.log(error);
 
-    var camb_data;
-    for (var i = 0; i < textData.length; i++){
-	var k_array = [];
-	var pk_array = [];
-	camb_data = parse_camb(textData[i]);
-	//Get P(k)
-	for (var pi = 0; pi < camb_data.length; pi++){
-	    pk_array.push(camb_data[pi][1]);
-	}
-	//Make k the first column
-	if (i == 0){
-	    for (var ki = 0; ki < camb_data.length; ki++){
-		k_array.push(camb_data[ki][0]);
-	    }
-	    pk_table.push(k_array);
-	}
-	// Add this data to table
-	//pk_table[0][i] = k
-	//pk_table[1...][i] = P(k)
-	pk_table.push(pk_array);
-    }
+    console.log("setting up pk table");
+    
+    pk_table = parse_pktable(textData);
+    param_table = parse_param(textData);
+    s8_table = parse_s8(textData);
+    
     run_interpolation();
 }
 
 //Load data and run plotting
-function run_pk_display(paramName){
+function run_pk_display(){
     console.log("Preparing for ", paramName);
-    if (paramName == 'Omh2'){
-	var filename_list = ["data/test_matterpower.dat", "data/omega_m_h2_005_matterpower.dat"];
-	param_table = [0.005,0.3];
+    if (paramName == 'Om' && !fix_omegamh2){
+	filename = 'data/pk_modeltype_fiducial_param_om.txt';
     }
-
+    if (paramName == 'Om' && fix_omegamh2){
+	filename = 'data/pk_modeltype_fixomch2_param_om.txt';
+    }
+    console.log("filename = ", filename);
+    
     var q = d3.queue();
-    for (var fi = 0; fi < filename_list.length; fi++){
-	q.defer(d3.text, filename_list[fi]);
-    }
-    q.awaitAll(process_pk_table);
+    q.defer(d3.text, filename);
+    console.log("filename = ", filename);
+
+    q.await(process_pk_table);
 }
 
 function get_k_pk_from_table(pk_table, index){
@@ -117,7 +107,8 @@ function interpolate(param, param_table, func_table) {
     //param_table is table of param values for which we've precomputed function
     //func_table is matrix of function evals (either P(k) or C(ell)
     //dimension is (1+len(param_table), len(P(k)))
-
+    //first column is k
+    
     [lower_temp, upper_temp] = determine_bounding_indices(param, param_table);
     lowerindex = lower_temp;
     upperindex = upper_temp;
@@ -128,8 +119,15 @@ function interpolate(param, param_table, func_table) {
     [weight1, weight2] = compute_weights(param, param_lower, param_upper);
 
     //+1 since first column is either k or ell
-    func_table_lower = func_table[lowerindex+1];
-    func_table_upper = func_table[upperindex+1];
+    //func_table_lower = func_table[lowerindex+1];
+    //func_table_upper = func_table[upperindex+1];
+    var func_table_lower = [];
+    var func_table_upper = [];
+    for (var i = 0; i < func_table.length; i++){
+	func_table_lower.push(func_table[i][lowerindex+1]);
+	func_table_upper.push(func_table[i][upperindex+1]);
+    }
+
     output_array = interpolate_between_two_lines(weight1, weight2, func_table_lower, func_table_upper);
     return output_array;
 };
