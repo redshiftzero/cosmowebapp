bb7e0f058c5e14e2a831ac33c831b50642af7a61
redshiftzero <jen@freedom.press>
2018-01-20 13:13:58 -0800

    Fix d3 axes: Sane tickmarks, add titles, axis labels

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,4 +1,4 @@
-function plot_axes(margin, svg_id, xlabel, xrange, ylabel, yrange){
+function plot_axes(margin, svg_id, xlabel, xrange, ylabel, yrange, title){
   d3.select(svg_id).selectAll("*").remove();
   var svg = d3.select(svg_id);
 	width = +svg.attr("width") - margin.left - margin.right;
@@ -6,25 +6,30 @@ function plot_axes(margin, svg_id, xlabel, xrange, ylabel, yrange){
 	g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   g.attr("width",width);
   g.attr("height",height);
+  console.log('width', width);
+  console.log('height', height);
 
   var x = d3.scaleLog().range([margin.left, width - margin.right]).domain(xrange);
 
   var y = d3.scaleLog().range([height - margin.top, margin.bottom]).domain(yrange);
 
   var xAxis = d3.axisBottom()
-    .scale(x);
+    .scale(x)
+    .ticks(5, ".4f");
 
   var yAxis = d3.axisLeft()
-    .scale(y);
+    .scale(y)
+    .ticks(5, ".0f");
 
   g.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis)
     .append("text")
-    .attr("x", 870)
+    .attr("fill", "#000")
     .attr("y", -4)
+    .attr("x", 400)
     .attr("dx", "0.71em")
-    .attr("font-size", "15px")
+    .attr("font-size", "12px")
     .attr("text-anchor", "middle")
     .text(xlabel);
 
@@ -32,12 +37,20 @@ function plot_axes(margin, svg_id, xlabel, xrange, ylabel, yrange){
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
     .call(yAxis)
     .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
+    .attr("fill", "#000")
+    .attr("y", 10)
+    .attr("x", 10)
     .attr("dy", "0.71em")
-    .attr("font-size", "15px")
+    .attr("font-size", "12px")
     .attr("text-anchor", "end")
     .text(ylabel);
+
+  g.append("text")
+    .attr("x", (width / 2) + 100)
+    .attr("y", 0 - (margin.top / 2) + 50)
+    .attr("text-anchor", "middle")
+    .style("font-size", "14px")
+    .text(title);
   return g
 }
 
